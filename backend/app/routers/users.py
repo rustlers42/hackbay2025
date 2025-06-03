27f69124d108ea6b43854ab277bf2ec1c3cb6d99
lks-hrsch <lkshrsch2001@outlook.com>
2025-06-03 15:37:47 +0200

    fix: UserDTO

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -22,6 +22,6 @@ async def get_leaderboard(session: Session = Depends(get_session)):
     Get the leaderboard
     """
     return [
-        UserDTO(id=user.id, email=user.email, username=user.username, bonus_points=user.bonus_points)
+        UserDTO(email=user.email, username=user.username, bonus_points=user.bonus_points)
         for user in session.exec(select(User).order_by(User.bonus_points.desc())).all()
     ]
