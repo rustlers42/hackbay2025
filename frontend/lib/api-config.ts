54b7d950bc10fc1971072fa56158f1710e599f97
yves147 <yves.j@outlook.de>
2025-06-03 14:54:20 +0200

    theos fehler korrigieren

--- frontend/lib/api-config.ts
+++ frontend/lib/api-config.ts
@@ -1,11 +1,10 @@
 const API_PORT = 8000;
 
-/*let host: string;
+let host: string;
 if (typeof window !== "undefined") {
   host = `${window.location.protocol}//${window.location.hostname}:${API_PORT}`;
 } else {
-  host = "http://lukas.dev.rustlers.xyz:8000";
-}*/
+  host = "http://lo.dev.rustlers.xyz:8000";
+}
 
-let host = "http://lukas.dev.rustlers.xyz:8000";
 export const BASE_API_URL = host;
